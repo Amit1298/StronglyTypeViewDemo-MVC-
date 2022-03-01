ade05c450629d74dd4c888c9a358b2ecdf7a327e
Amit <amitkr24031995@gmail.com>
2022-03-01 22:42:45 +0530

    Adding StronglyTypeViewDemo

---
+++ StronglyTypeViewDemo/Controllers/HomeController.cs
@@ -0,0 +1,25 @@
+﻿using StronglyTypeViewDemo.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace StronglyTypeViewDemo.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            Employee emp1 = new Employee();
+            emp1.id = 12;
+            emp1.Name = "Amit";
+            emp1.Age = 23;
+
+            ViewData["Var1"] = emp1;
+            ViewBag.Var2 = emp1;
+            return View();
+        }
+    }
+}
